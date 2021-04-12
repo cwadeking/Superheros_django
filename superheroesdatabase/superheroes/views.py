875947cca4a7113992ed9a91b11459fe38105cf0
cwadeking <charles.w.king.cwk@gmail.com>
2021-04-11 20:39:15 -0500

    created template folder with app folder inside and first view.  created superhero model as well.

---
+++ superheroesdatabase/superheroes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Superhero
+# Create your views here.
+
+
+def index(request):
+    all_superheroes = Superhero.objects.all()
+    context = {
+        'all_superheroes': all_superheroes
+    }
+    return render(request, 'superheros/index.html', context)
